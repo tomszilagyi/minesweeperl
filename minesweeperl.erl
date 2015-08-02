443a1e02cd0fd4d220a4679acd377c9b0714a6a1
Tom Szilagyi <tamas.szilagyi@klarna.com>
2015-08-02 19:06:19 +0200

    Enhance data model by storing neighbours; add unit tests

--- minesweeperl.erl
+++ minesweeperl.erl
@@ -1,49 +1,51 @@
 -module(minesweeperl).
 -author('Tom Szilagyi <tomszilagyi@gmail.com>').
--export([new_game/3]).
+-export([new_game/3, new_game/4]).
 
--compile([export_all]). % DEBUG
-
--record(opts,
-        { level
-        , dims
-        , n_mines
-        }).
+-include_lib("eunit/include/eunit.hrl").
 
 -record(field,
         { is_mine    = false
         , is_covered = true
         , is_flagged = false
-        , n_borders
+        , neighbours
         , neighbour_mines
         }).
 
--record(state,
-        { opts
-        , board
+-record(board,
+        { dims
+        , n_mines
+        , fields
         }).
 
+new_game(Rows, Cols, _Mines) when Rows < 2; Cols < 2 ->
+    throw(badarg);
 new_game(Rows, Cols, Mines) ->
-    Dims = {Rows, Cols},
-    Board0 = array:new([{size, Rows*Cols}, {default, #field{}}]),
     MineSeqList = generate_mines(Rows*Cols, Mines),
-    Board1 = array:map(fun(Seq, Field) ->
-                               case lists:member(Seq, MineSeqList) of
-                                   true -> Field#field{is_mine = true};
-                                   false -> Field
-                               end
-                       end, Board0),
-    Board2 = array:map(fun(Seq, Field) ->
-                               Pos = seq_to_pos(Seq, Dims),
-                               Field#field{n_borders = n_borders(Pos, Dims)}
-                       end, Board1),
-    Board3 = array:map(fun(Seq, Field) ->
-                               Pos = seq_to_pos(Seq, Dims),
-                               NMines = neighbour_mines(Pos, Dims, MineSeqList),
-                               Field#field{neighbour_mines = NMines}
-                       end, Board2),
-    #state{opts = #opts{dims = Dims, n_mines = Mines},
-           board = Board3}.
+    new_game(Rows, Cols, Mines, MineSeqList).
+
+%% partially or fully specified list of mines:
+new_game(Rows, Cols, Mines, MineSeqList0) when length(MineSeqList0) < Mines ->
+    MineSeqList = generate_mines(Rows*Cols, Mines, MineSeqList0),
+    new_game(Rows, Cols, Mines, MineSeqList);
+new_game(Rows, Cols, Mines, MineSeqList) ->
+    Dims = {Rows, Cols},
+    Fields0 = array:new([{size, Rows*Cols}, {default, #field{}}]),
+    Fields1 = array:map(fun(Seq, Field) ->
+                                case lists:member(Seq, MineSeqList) of
+                                    true -> Field#field{is_mine = true};
+                                    false -> Field
+                                end
+                        end, Fields0),
+    Fields2 = array:map(fun(Seq, Field) ->
+                                Pos = seq2pos(Seq, Dims),
+                                Field#field{neighbours = neighbours(Pos, Dims)}
+                        end, Fields1),
+    Fields3 = array:map(fun(_Seq, Field) ->
+                                NMines = neighbour_mines(Field, Fields2),
+                                Field#field{neighbour_mines = NMines}
+                        end, Fields2),
+    #board{dims = Dims, n_mines = Mines, fields = Fields3}.
 
 
 generate_mines(Fields, N) when N >= Fields -> throw(badarg);
@@ -59,42 +61,84 @@ generate_mines(Fields, N, Mines) ->
     end.
 
 
-seq_to_pos(Seq, {_Rows, Cols}) ->
+seq2pos(Seq, {_Rows, Cols}) ->
     X = Seq div Cols,
     Y = Seq rem Cols,
     {X, Y}.
 
-pos_to_seq({Px, Py}, {_Rows, Cols}) ->
-    Py + Cols * Px.
+pos2seq({Px, Py}, {_Rows, Cols}) -> Py + Cols * Px.
+
+posl2seql(PosList, Dim) -> [pos2seq(Pos, Dim) || Pos <- PosList].
 
 
-n_borders({Px, Py}, {Rows, Cols}) -> n_b1({Px, Py}, {Rows-1, Cols-1}).
-n_b1({0, 0}, {_, _}) -> 3;
-n_b1({0, C}, {_, C}) -> 3;
-n_b1({R, 0}, {R, _}) -> 3;
-n_b1({R, C}, {R, C}) -> 3;
-n_b1({0, _}, {_, _}) -> 5;
-n_b1({R, _}, {R, _}) -> 5;
-n_b1({_, 0}, {_, _}) -> 5;
-n_b1({_, C}, {_, C}) -> 5;
-n_b1({_, _}, {_, _}) -> 8.
+neighbours({Px, Py}, {Rows, Cols}) ->
+    ns_1({Px, Py}, {Rows-1, Cols-1}, {Rows, Cols}).
 
-true1(true)  -> 1;
-true1(false) -> 0.
+ns_1({0, 0}, {_, _}, Dim) -> % top left
+    posl2seql([{0, 1}, {1, 0}, {1, 1}], Dim);
+ns_1({0, C}, {_, C}, Dim) -> % top right
+    posl2seql([{0, C-1}, {1, C-1}, {1, C}], Dim);
+ns_1({R, 0}, {R, _}, Dim) -> % bottom left
+    posl2seql([{R-1, 0}, {R-1, 1}, {R, 1}], Dim);
+ns_1({R, C}, {R, C}, Dim) -> % bottom right
+    posl2seql([{R-1, C-1}, {R-1, C}, {R, C-1}], Dim);
+ns_1({0, Py}, {_, _}, Dim) -> % top row
+    posl2seql([{0, Py-1}, {0, Py+1}, {1, Py-1}, {1, Py}, {1, Py+1}], Dim);
+ns_1({R, Py}, {R, _}, Dim) -> % bottom row
+    posl2seql([{R-1, Py-1}, {R-1, Py}, {R-1, Py+1}, {R, Py-1}, {R, Py+1}], Dim);
+ns_1({Px, 0}, {_, _}, Dim) -> % left column
+    posl2seql([{Px-1, 0}, {Px-1, 1}, {Px, 1}, {Px+1, 0}, {Px+1, 1}], Dim);
+ns_1({Px, C}, {_, C}, Dim) -> % right column
+    posl2seql([{Px-1, C-1}, {Px-1, C}, {Px, C-1}, {Px+1, C-1}, {Px+1, C}], Dim);
+ns_1({Px, Py}, {_, _}, Dim) -> % inner square
+    posl2seql([{Px-1, Py-1}, {Px-1, Py}, {Px-1, Py+1}, {Px, Py-1}, {Px, Py+1},
+               {Px+1, Py-1}, {Px+1, Py}, {Px+1, Py+1}], Dim).
 
-has_mine({Px, _Py}, {_Rows, _Cols}, _MineSeqList) when Px < 0     -> false;
-has_mine({Px, _Py}, {Rows, _Cols},  _MineSeqList) when Px >= Rows -> false;
-has_mine({_Px, Py}, {_Rows, _Cols}, _MineSeqList) when Py < 0     -> false;
-has_mine({_Px, Py}, {_Rows, Cols},  _MineSeqList) when Py >= Cols -> false;
-has_mine(Pos, Dims, MineSeqList) ->
-    lists:member(pos_to_seq(Pos, Dims), MineSeqList).
 
-neighbour_mines({Px, Py}, Dims, MineSeqList) ->
-    true1(has_mine({Px-1, Py-1}, Dims, MineSeqList)) +
-    true1(has_mine({Px-1, Py},   Dims, MineSeqList)) +
-    true1(has_mine({Px-1, Py+1}, Dims, MineSeqList)) +
-    true1(has_mine({Px,   Py-1}, Dims, MineSeqList)) +
-    true1(has_mine({Px,   Py+1}, Dims, MineSeqList)) +
-    true1(has_mine({Px+1, Py-1}, Dims, MineSeqList)) +
-    true1(has_mine({Px+1, Py},   Dims, MineSeqList)) +
-    true1(has_mine({Px+1, Py+1}, Dims, MineSeqList)).
+neighbour_mines(#field{neighbours = Nbs}, Fields) ->
+    NbFs = [array:get(Seq, Fields) || Seq <- Nbs],
+    lists:foldl(fun(#field{is_mine = true}, Cnt) -> Cnt+1;
+                   (#field{}, Cnt) -> Cnt
+                end, 0, NbFs).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Tests
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+newgame_test() ->
+    %% Create a board with 3 rows and 4 cols with 5 pre-determined mines:
+    %%   0, 1,*2,*3
+    %%   4,*5, 6,*7
+    %%   8,*9,10,11
+    Board = new_game(3, 4, 5, [2,3,5,7,9]),
+    #board{dims = {3, 4}, n_mines = 5, fields = Fields} = Board,
+    F5 = array:get(5, Fields),
+    F6 = array:get(6, Fields),
+    ?assertEqual(true, F5#field.is_mine),
+    ?assertEqual(false, F6#field.is_mine),
+    ?assertEqual(2, neighbour_mines(F5, Fields)),
+    ?assertEqual(5, neighbour_mines(F6, Fields)).
+
+
+neighbours_test() ->
+    %% Field seq numbers:
+    %%   0, 1, 2, 3
+    %%   4, 5, 6, 7
+    %%   8, 9,10,11
+
+    %% corners:
+    ?assertEqual([1,4,5], neighbours({0,0}, {3,4})),
+    ?assertEqual([2,6,7], neighbours({0,3}, {3,4})),
+    ?assertEqual([4,5,9], neighbours({2,0}, {3,4})),
+    ?assertEqual([6,7,10], neighbours({2,3}, {3,4})),
+    %% sides:
+    ?assertEqual([0,2,4,5,6], neighbours({0,1}, {3,4})),
+    ?assertEqual([1,3,5,6,7], neighbours({0,2}, {3,4})),
+    ?assertEqual([0,1,5,8,9], neighbours({1,0}, {3,4})),
+    ?assertEqual([2,3,6,10,11], neighbours({1,3}, {3,4})),
+    ?assertEqual([4,5,6,8,10], neighbours({2,1}, {3,4})),
+    ?assertEqual([5,6,7,9,11], neighbours({2,2}, {3,4})),
+    %% inner squares:
+    ?assertEqual([0,1,2,4,6,8,9,10], neighbours({1,1}, {3,4})),
+    ?assertEqual([1,2,3,5,7,9,10,11], neighbours({1,2}, {3,4})).
